aecf32a2d01a49b21a2caf5ae21a209ae7a40569
jonathanchen1999 <57915885+jonathanchen1999@users.noreply.github.com>
2019-12-11 00:00:51 -0500

    Update jeopardy.py

--- jeopardy.py
+++ jeopardy.py
@@ -1,23 +1,21 @@
 import csv
 
 class Categories():
-  def __init__(self,sports,politics,popculture,tech,food):
+  def __init__ (self,sports,politics,popculture,tech,food):
       self.sports=sports
       self.politics=politics
       self.popculture=popculture
       self.tech=tech
       self.food=food
-      
+
 
 def main():
-  hundred=[]
-  
-  
-  with open('final_project_questions.csv') as handle:
-    reader=csv.DictReader(handle, delimiter=',')
-     for row in reader:
-      questions=Categories(row['Sports],row['Politics'],row['Pop Culture'],row['Technology'],row['Food']
-      hundred.append[questions.sports[0]
-      
-  user_input=''
-  
+
+    with open('final_project_questions.csv', encoding="UTF-8") as handle:
+        reader=csv.DictReader(handle, delimiter=',')
+        for row in reader:
+            questions = Categories(row['Sports'],row['Politics'],row['Pop Culture'],row['Technology'],row['Food'])
+            print(categories.sports)
+              
+if __name__ == '__main__':
+    main()
